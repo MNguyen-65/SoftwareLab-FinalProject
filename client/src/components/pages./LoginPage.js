c359852c36db12092f8e269513ebc4b24e14c64b
Steven Shan <60268606+Steven-Shan@users.noreply.github.com>
2023-04-02 12:35:42 -0500

    Added styling to this, make sure to delete any unnecessary loginpage to prevent hiccups when using

--- client/src/components/pages./LoginPage.js
+++ client/src/components/pages./LoginPage.js
@@ -1,20 +1,18 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-
-import '../../App.css'
+import './LoginPage.css'
 
 export default function SignInPage() {
     return (
-        <div className="text-center m-5-auto">
-            <h2>Sign in to us</h2>
-            <form action="/home">
-                <p>
-                    <label>Username or email address</label><br/>
-                    <input type="text" name="first_name" required />
+        <div className="text-center m-5-auto" id="loginlanding">
+            <h2 id="loginsign">Sign in to us</h2>
+            <form action="/home" className="login-form"> 
+                <p className="text-center">
+                    <label style={{ color: 'red', textAlign: 'center', fontFamily: 'Arial, Helvetica, sans-serif'}}>Username or email address</label><br/>
+                    <input style={{textAlign: 'center'}} type="text" name="first_name" required />
                 </p>
                 <p>
-                    <label>Password</label>
-                    <Link to="/forget-password"><label className="right-label">Forget password?</label></Link>
+                    <label style={{ color: 'red', textAlign: 'center', fontFamily: 'Arial, Helvetica, sans-serif'}}> Password</label>
                     <br/>
                     <input type="password" name="password" required />
                 </p>
@@ -23,9 +21,12 @@ export default function SignInPage() {
                 </p>
             </form>
             <footer>
-                <p>First time? <Link to="/register">Create an account</Link>.</p>
-                <p><Link to="/">Back to Homepage</Link>.</p>
+                <p id="linkedpageheader">First time? </p>
+                <p id="linkedpage" style={{fontFamily: 'Arial, Helvetica, sans-serif', color: 'lightgreen'}}> <Link to="/register" style={{color: 'rgb(51, 205, 51)'}}>Create an account</Link></p>
+                <p id="linkedpage" style={{fontFamily: 'Arial, Helvetica, sans-serif', color: 'lightgreen'}}><Link to="/" style={{color: 'rgb(51, 205, 51)'}}>Back to Homepage</Link>.</p>
+                <p id="linkedpage" style={{fontFamily: 'Arial, Helvetica, sans-serif', color: 'lightgreen'}}><Link to="/forget-password" style={{color: 'rgb(51, 205, 51)'}}> Forget password?</Link></p>
             </footer>
         </div>
     )
 }
+
